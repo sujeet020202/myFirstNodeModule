645c540ff2756fea70844558b0c742c856e29541
sujeet <sujeet.dagor@gmail.com>
2016-01-06 18:02:20 +0530

    node

--- hello.js
+++
@@ -1,6 +0,0 @@
-function myfirstmodule(fn) {
-  fn(); // do the function right now
-  return setInterval.apply(this, arguments); // defer to setInterval
-}
-
-module.exports = myfirstmodule; // let me be required
